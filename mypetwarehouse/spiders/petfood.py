86065433d088825efa824e8a36f8ce9b5f420849
nitin j <nitinjoseph@outlook.com>
2021-03-19 22:55:17 -0400

    Updated Spider with User Agent

--- mypetwarehouse/spiders/petfood.py
+++ mypetwarehouse/spiders/petfood.py
@@ -7,7 +7,13 @@ from scrapy.spiders import CrawlSpider, Rule
 class PetfoodSpider(CrawlSpider):
     name = 'petfood'
     allowed_domains = ['mypetwarehouse.com.au']
-    start_urls = ['https://www.mypetwarehouse.com.au/dog-food']
+
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.mypetwarehouse.com.au/dog-food', headers={
+            'User-Agent': self.user_agent
+        })
 
     rules = (
         #Rule(LinkExtractor(restrict_xpaths="(//div[@class ='item-grid col-xs-12 col-sm-6 col-md-4 col-lg-3']/div/a"), callback='parse_item', follow=True)
@@ -20,6 +26,6 @@ class PetfoodSpider(CrawlSpider):
         yield {
             'Product': response.xpath("//h1/text()").get(),
             'price': response.xpath("//span[@class ='text-price']/text()").get(),
-            ##'book_url': response.url,
-            #'book_category':response.xpath("//ul[@class='breadcrumb']/li[3]/a/text()").get(),
+            'product_url': response.url,
+            
         }
