f53dfbedc4593421b815d8516b2eaa5206980737
nitin j <nitinjoseph@outlook.com>
2021-03-19 23:12:31 -0400

    commented how spider is configured

--- mypetwarehouse/spiders/petfood.py
+++ mypetwarehouse/spiders/petfood.py
@@ -6,20 +6,22 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class PetfoodSpider(CrawlSpider):
     name = 'petfood'
-    allowed_domains = ['mypetwarehouse.com.au']
+    allowed_domains = ['mysetwarehouse.com.au']
 
+    ##User_agent can also be definted from the setting file
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
 
     def start_requests(self):
-        yield scrapy.Request(url='https://www.mypetwarehouse.com.au/dog-food', headers={
+        ##to prevent abuse, domain crawled has been intentionally modified
+        yield scrapy.Request(url='https://www.mysetwarehouse.com.au/dog-food', headers={
             'User-Agent': self.user_agent
         })
 
     rules = (
-        #Rule(LinkExtractor(restrict_xpaths="(//div[@class ='item-grid col-xs-12 col-sm-6 col-md-4 col-lg-3']/div/a"), callback='parse_item', follow=True)
+        #link extractors below uses Xpath to identify the elements on the page and extract the link and then follow
         Rule(LinkExtractor(restrict_xpaths="//div[@class ='list-item-grid col-xs-6 col-sm-4 col-md-3']/div/a"), callback='parse_item', follow=True),
         Rule(LinkExtractor(restrict_xpaths="(//td[@valign ='top'])[4]/a"), callback='parse_item', follow=True),
-        #Rule(LinkExtractor(restrict_xpaths="//span[@class='button-text'])[last()]")),
+
     )
 
     def parse_item(self, response):
